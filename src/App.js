58a82abc0f1ecbd895d4c2e6add623dda04234e8
Daniel <walidan@wp.pl>
2021-01-09 12:15:47 +0100

    Even numbers filter

--- src/App.js
+++ src/App.js
@@ -18,9 +18,11 @@ function Numbers(props) {
   const numbersDouble = numbers.map((number) => (<li key={number}>{number * 2}</li>));
   return (
     <div className="numbers">
+      <h2> Liczby </h2> 
       <ul> 
           {numbersLi}
       </ul>
+      <h2>Podwojone </h2>
       <ul>
           {numbersDouble}
       </ul>
@@ -28,13 +30,49 @@ function Numbers(props) {
   )
 } 
 
+
+class EvenNumbers extends React.Component {
+  render() {
+
+    const numbers = this.props.numbers
+    console.log(numbers)
+    const numbersFiltered = numbers.filter(number => number % 2 === 0).map(number => <li key={number}>{number}</li>)
+    console.log(numbersFiltered)
+    return (
+      <div>
+        <h2>Tylko parzyste </h2> 
+        <ul>
+          {numbersFiltered}
+        </ul>
+      </div>
+    )
+  }
+}
+
+function EvenNumbersConditional(props) {
+  const numbers = props.numbers;
+  const numbersLi = numbers.map(number => number % 2 === 0 && <li key={number}>{number}</li>)
+
+  const showMessage = numbers.length > 0; // false
+  return (
+    <div>
+      {showMessage && <h2> Tylko parzyste (conditional)</h2>}
+      <ul>
+        {numbersLi}
+      </ul>
+    </div>
+  )
+}
+
 function App() {
-  const numbers = [1, 2, 3, 4]
+  const numbers = [3, 5, 7]
   
   return (
     <div className="app">
       {/* <ClassNumbers  nums={numbers}/> */}
       <Numbers numbers={numbers} />
+      <EvenNumbers numbers={numbers}/>
+      <EvenNumbersConditional numbers={numbers}/>
      </div>
   );
 }
