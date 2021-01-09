cfcd4e619aa2dd913f13ffbf6f8a77f6b8e0d094
Daniel <walidan@wp.pl>
2021-01-09 11:53:36 +0100

    Example with numbers

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+// class ClassNumbers extends React.Component {
+  
+//   render() {
+//     console.log(this.props.nums);
+//     return <ul>  
+      
+//     </ul>
+//   }
+// }
+
+function Numbers(props) {
+  const numbers = props.numbers
+  const numbersLi = numbers.map(number => (<li className="cos" key={number}> {number} </li>) );
+  const numbersDouble = numbers.map((number) => (<li key={number}>{number * 2}</li>));
+  return (
+    <div className="numbers">
+      <ul> 
+          {numbersLi}
+      </ul>
+      <ul>
+          {numbersDouble}
+      </ul>
+    </div>
+  )
+} 
 
 function App() {
+  const numbers = [1, 2, 3, 4]
+  
   return (
-    <> </>
+    <div className="app">
+      {/* <ClassNumbers  nums={numbers}/> */}
+      <Numbers numbers={numbers} />
+     </div>
   );
 }
 
